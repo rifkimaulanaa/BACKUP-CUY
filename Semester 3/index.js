031a31ac3e1dc9c6f8380e39c488b9a0cc099bc5
cousin-rifki <rifkiibul@gmail.com>
2023-12-17 10:51:36 +0700

    backup

---
+++ Semester 3/index.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const app = express()
+const port = 3000
+const bodyParser = require("body-parser")
+const db = require("./connection")
+const response = require("./response")
+
+app.use(bodyParser.json())
+
+app.get("/", (req, res) => {
+  response(200, "ini data", "ini message", res)
+})
+
+app.get("/mahasiswa", (req, res) => {
+  response(200, "mahasiwa get list", res)
+})
+
+app.get("/mahasiswa/:nim", (req, res) => {
+  const nim = req.params.nim
+  response(200, `spesifik mahasiswa by id ${nim}`, res)
+})
+
+app.post("/mahasiswa", (req, res) => {
+  response(200, "INI POSTING", res)
+})
+
+app.put("/mahasiswa", (req, res) => {
+  response(200, "INI PUT ATAU UPDATE DATA", res)
+})
+
+app.delete("/mahasiswa", (req, res) => {
+  response(200, "INI DELETE DATA", res)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
